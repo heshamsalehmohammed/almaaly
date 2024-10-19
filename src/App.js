e8dd1ec2bbafc2d86fe1d8bf1e29cfb2e5794365
hesham.saleh <hesham.saleh@solve-it-solutions.com>
2024-10-19 03:47:18 +0300

    finalize page1

--- src/App.js
+++ src/App.js
@@ -10,15 +10,22 @@ import {
   ScrollControls,
   Scroll,
   useScroll,
+  Html,
 } from "@react-three/drei";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "./bootstrap-addon.css";
+
 import "./App.css";
 import Text from "./Text";
 import Effects from "./Effects";
 import { isPositiveNumber } from "./packages/PageScroll/utils";
+import { Slide,Fade,JackInTheBox,Roll,Zoom,Bounce } from "react-awesome-reveal";
+import SocialIcons from './components/SocilaIcons';
+import { PrimeReactProvider } from "primereact/api";
+import { Button } from 'primereact/button';   
 
 gsap.registerPlugin(ScrollTrigger);
 function Item({ url, scale, ...props }) {
@@ -59,14 +66,18 @@ function Item({ url, scale, ...props }) {
   );
 }
 
-function CanvasScrolledItems() {
+function CanvasScrolledItems({ setLogoScreenPosition }) {
   const { width: w, height: h } = useThree((state) => state.viewport);
   const [hovered, hover] = useState(false);
 
   const mouse = useRef([0, 0]);
   return (
     <Scroll>
-      <LogoText mouse={mouse} hover={hover} />
+      <LogoText
+        mouse={mouse}
+        hover={hover}
+        setLogoScreenPosition={setLogoScreenPosition}
+      />
       <Item
         url="/images/1.jpg"
         scale={[w / 3, w / 3, 1]}
@@ -169,8 +180,31 @@ const AnimatedH1 = ({
   );
 };
 
-const HtmlScrolledItems = () => {
+const HtmlScrolledItems = ({ logoScreenPosition }) => {
   const scroll = useScroll(); // Get the scroll API
+  /*   const { viewport,size } = useThree();
+  const height = viewport.height; */
+
+  const { viewport, size, camera } = useThree();
+
+  // Calculate font size
+  const fontSize = 20 * Math.min(1, viewport.width / 128);
+  const fontResolution = 1000;
+  const scale = fontSize / fontResolution;
+
+  // Calculate lower Y offset using boundingBox.yMin
+  const lowerYOffset = 2 * -486 * scale;
+
+  // Set text position
+  const positionY = viewport.height / 4 + 1;
+  const lowerYScene = positionY + lowerYOffset;
+
+  // Convert to screen Y
+  const vector = new THREE.Vector3(0, lowerYScene, 0);
+  vector.project(camera);
+
+  // Convert NDC to screen Y coordinates
+  const screenY = (-vector.y * 0.5 + 0.5) * size.height;
 
   const scrollToSection = (section) => {
     // Set scroll position manually
@@ -180,7 +214,42 @@ const HtmlScrolledItems = () => {
   };
 
   return (
-    <Scroll html style={{ width: "100%", zIndex: 10 }}>
+    <Scroll html style={{ width: "100vw", zIndex: 10 }}>
+      <div
+        className="row justify-content-center vw-100 page-1 pt-5 pt-sm-5"
+        style={{
+          position: "absolute",
+          top: `${screenY}px`,
+        }}
+      >
+        <div className="col-12 pt-1 pt-sm-5 mt-sm-5">
+          <div
+            className="welcome-box"
+            style={{
+              color: "#000",
+              fontWeight: "bolder",
+              textShadow: "0 2px 20px #000000e0",
+            }}
+          >
+            <Slide direction="up">
+              <p className="col-sm-6 col-md-8 col-lg-6 desc pt-2 pb-0 mt-4 mb-1 ps-5 pe-5 color-inherit">
+                Embark on a journey of educational excellence with Almaaly.
+                Discover our rich history and commitment to nurturing future
+                leaders.
+              </p>
+            </Slide>
+          </div>
+        
+        </div>
+        <div class="col-6 pt-5 d-flex flex-wrap justify-content-start">
+          <Fade cascade>
+                    <Button className="pe-4 m-2" iconPos="right" style={{width: '180px',borderRadius:"25px",background:'#000080',borderColor:"#000080",boxShadow:'none'}} label="Contact Us" icon="fa-solid fa-arrow-right-long ms-0" />
+
+        
+              <Button className="pe-4 m-2" iconPos="right" style={{width: '180px',borderRadius:"25px",background:'#fff',borderColor:"#000080",boxShadow:'none',color:'rgb(15 23 42 / 1)'}} label="Join Us" icon="fa-solid fa-arrow-right-long ms-0" />
+  </Fade>
+            </div>
+      </div>
       <AnimatedH1
         id="h1-all"
         style={{
@@ -272,23 +341,19 @@ const HtmlScrolledItems = () => {
   );
 };
 
-function LogoText({ hover }) {
-
+function LogoText({ hover, setLogoScreenPosition }) {
   const visible = useRef(false);
   const ref = useIntersect((isVisible) => (visible.current = isVisible));
-  const { viewport, size } = useThree()
+  const { viewport } = useThree();
   const height = viewport.height;
 
+  const TextScaleFactor = Math.min(1, viewport.width / 128);
 
-  const TextScaleFactor = Math.min(1, viewport.width / 128)
-
-  console.log("TextScaleFactor: ",TextScaleFactor)
-
-  useFrame((state,delta) => {
+  useFrame((state, delta) => {
     if (ref.current) {
       ref.current.position.y = THREE.MathUtils.damp(
         ref.current.position.y,
-        visible.current ? 0 : height / 4 + 1,
+        height / 4 + 1,
         2,
         delta
       );
@@ -297,34 +362,33 @@ function LogoText({ hover }) {
         state.mouse.x * 2,
         0.1
       );
-/*       ref.current.rotation.x = THREE.MathUtils.lerp(
+      /*       ref.current.rotation.x = THREE.MathUtils.lerp(
         ref.current.rotation.x,
         state.mouse.y / 2,
         0.1
       ); */
       ref.current.rotation.y = 0.01;
-
     }
   });
 
   return (
-    <group ref={ref} renderOrder={4}> {/* Assign to layer 1 */}
+    <group ref={ref} renderOrder={4}>
       <Text
-        size={20 * TextScaleFactor}
+        fontSize={20 * TextScaleFactor}
         height={5}
-        scale={[1,1, 0.1]}
+        scale={[1, 1, 0.1]}
         onPointerOver={() => hover(true)}
         onPointerOut={() => hover(false)}
         color="#ffffff" // Use a bright color for better Bloom effect'
         emissive="#ffffff" // Bright enough to trigger bloom
         emissiveIntensity={2.0} // Higher than the bloom threshold
+        setLogoScreenPosition={setLogoScreenPosition}
       >
         Almaaly
       </Text>
     </group>
   );
 }
-
 
 /* const MINIMAL_DELTA_Y_DIFFERENCE = 1;
 let isScrolling = false;
@@ -383,28 +447,30 @@ const FullPageScroll = ()=>{
   return null;
 } */
 
-
 const App = () => {
+  const [logoScreenPosition, setLogoScreenPosition] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     document.body.style.cursor =
       "url('https://raw.githubusercontent.com/chenglou/react-motion/master/demos/demo8-draggable-list/cursor.png') 39 39, auto";
   }, []);
 
-
   return (
     <PageProvider>
       <Header />
       <Canvas linear dpr={[1, 2]} camera={{ fov: 100, position: [0, 0, 30] }}>
-      <ambientLight intensity={0.5} />
+        <ambientLight intensity={0.5} />
         <ThreeBackgroundVideo />
-        
+
         <Effects />
-       <ScrollControls pages={5}>
-          <CanvasScrolledItems />
-          <HtmlScrolledItems />
-        </ScrollControls>   
+        <ScrollControls pages={5}>
+          <CanvasScrolledItems setLogoScreenPosition={setLogoScreenPosition} />
+          <HtmlScrolledItems logoScreenPosition={logoScreenPosition} />
+        </ScrollControls>
       </Canvas>
+      <SocialIcons/>
     </PageProvider>
+    
   );
 };
 export default App;
--- src/Text.js
+++ src/Text.js
@@ -1,38 +1,82 @@
 import * as THREE from "three";
-import React, { forwardRef, useLayoutEffect, useRef, useMemo, useEffect } from 'react'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import React, {
+  forwardRef,
+  useLayoutEffect,
+  useRef,
+  useMemo,
+  useEffect,
+} from "react";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
-import { extend, useLoader } from '@react-three/fiber'
+import { extend, useLoader, useThree } from "@react-three/fiber";
 
-extend({TextGeometry })
+extend({ TextGeometry });
 
+const Text = forwardRef(
+  (
+    {
+      children,
+      vAlign = "center",
+      hAlign = "center",
+      fontSize,
+      height,
+      scale,
+      color = "#000000",
+      setLogoScreenPosition,
+      ...props
+    },
+    ref
+  ) => {
+    const font = useLoader(
+      FontLoader,
+      "/three-fonts/minimized/Fira Sans Eight_Regular.json"
+    );
+    const config = useMemo(
+      () => ({ font, size: fontSize, height }),
+      [font, fontSize, height]
+    );
+    const { size, camera } = useThree();
 
-const Text = forwardRef(({ children, vAlign = 'center', hAlign = 'center', size,height,scale, color = '#000000', ...props }, ref) => {
-    const font = useLoader(FontLoader, '/three-fonts/minimized/Fira Sans Eight_Regular.json')
-    const config = useMemo(() => ({ font, size, height }), [font,size,height])
-    const mesh = useRef()
+    const mesh = useRef();
     useLayoutEffect(() => {
-      const size = new THREE.Vector3()
-      mesh.current.geometry.computeBoundingBox()
-      mesh.current.geometry.boundingBox.getSize(size)
-      mesh.current.position.x = hAlign === 'center' ? -size.x / 2 : hAlign === 'right' ? 0 : -size.x
-      mesh.current.position.y = vAlign === 'center' ? -size.y : vAlign === 'top' ? 0 : -size.y
-    }, [children,size,height])
+      const bbSize = new THREE.Vector3();
+      mesh.current.geometry.computeBoundingBox();
+      mesh.current.geometry.boundingBox.getSize(bbSize);
+      mesh.current.position.x =
+        hAlign === "center"
+          ? -bbSize.x / 2
+          : hAlign === "right"
+          ? 0
+          : -bbSize.x;
+      mesh.current.position.y =
+        vAlign === "center" ? -bbSize.y : vAlign === "top" ? 0 : -bbSize.y;
 
+/*       if (size) {
+        const vector = new THREE.Vector3();
+        mesh.current.getWorldPosition(vector);
+        vector.project(camera);
 
+        const screenX = (vector.x * 0.5 + 0.5) * size.width;
+        const screenY = (-vector.y * 0.5 + 0.5) * size.height;
+
+        // Update the position in state
+        setLogoScreenPosition({ x: screenX, y: screenY });
+      } */
+    }, [children, size, fontSize, height]);
 
     return (
       <group ref={ref} {...props} scale={scale}>
-        <mesh ref={mesh} 
-        >
+        <mesh ref={mesh}>
           <textGeometry args={[children, config]} />
-          <meshStandardMaterial color={color} 
-          emissive="#ffffff"
-          emissiveIntensity={2.0}
+          <meshStandardMaterial
+            color={color}
+            emissive="#ffffff"
+            emissiveIntensity={2.0}
           />
         </mesh>
       </group>
-    )
-  })
-export default Text
+    );
+  }
+);
+export default Text;
