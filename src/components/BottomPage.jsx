29dbd7d57f27a0b4b3226b0912a0acfa733ed5c0
hesham.saleh <hesham.saleh@solve-it-solutions.com>
2024-10-22 13:22:24 +0300

    some style

--- src/components/BottomPage.jsx
+++ src/components/BottomPage.jsx
@@ -339,7 +339,7 @@ const BottomPage = forwardRef(({ scrollAreaRef }, ref) => {
           ))}
         </div>
         <div className="row w-100 p-1 mt-1 mb-5 justify-content-center">
-          <div className="col-12 col-md-8 col-lg-7">
+          <div className="col-12 col-md-8 col-lg-8 col-xl-7">
             <MainYouTubeEmbedWithAnimation
               videoId="dQw4w9WgXcQ"
               scrollAreaRef={scrollAreaRef}
@@ -380,7 +380,7 @@ const BottomPage = forwardRef(({ scrollAreaRef }, ref) => {
         </div>
         <div className="position-relative w-100">
           <div className="row p-3 mt-5 mb-5 pt-5 pb-5 justify-content-center">
-            <div className="col-11 col-md-6 col-lg-5 pt-5 pb-5">
+            <div className="col-11 col-md-6 col-lg-6 col-xl-5 pt-5 pb-5">
               <NormalYouTubeEmbedWithAnimation
                 videoId="dQw4w9WgXcQ"
                 ref={video1Ref}
@@ -388,7 +388,7 @@ const BottomPage = forwardRef(({ scrollAreaRef }, ref) => {
             </div>
           </div>
           <div className="row p-3 mt-5 mb-5  pt-5 pb-5 justify-content-center">
-            <div className="col-11 col-md-6 col-lg-5 pt-5 pb-5">
+            <div className="col-11 col-md-6 col-lg-6 col-xl-5 pt-5 pb-5">
               <NormalYouTubeEmbedWithAnimation
                 videoId="dQw4w9WgXcQ"
                 ref={video2Ref}
@@ -396,7 +396,7 @@ const BottomPage = forwardRef(({ scrollAreaRef }, ref) => {
             </div>
           </div>
           <div className="row p-3 mt-5 mb-5  pt-5 pb-5 justify-content-center" ref={lastRowRef}>
-            <div className="col-11 col-md-6 col-lg-5 pt-5 pb-5">
+            <div className="col-11 col-md-6 col-lg-6 col-xl-5 pt-5 pb-5">
               <NormalYouTubeEmbedWithAnimation
                 videoId="dQw4w9WgXcQ"
                 ref={video3Ref}
