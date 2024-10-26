9e7ef82363e7b1b27c754048c68da65c1e3eca9c
hesham.saleh <hesham.saleh@solve-it-solutions.com>
2024-10-26 19:37:17 +0300

    ehhance

--- src/components/Sections/StudentsGallarySection.jsx
+++ src/components/Sections/StudentsGallarySection.jsx
@@ -48,81 +48,91 @@ const StudentsGallarySection = forwardRef(({ scrollAreaRef }, ref) => {
     }
   };
 
+  const calculateAnimationPropsBasedOnMEdiaQuery = (animationProps) => {
+    if (window.innerWidth > 1200)
+      return animationProps;
 
-  const calculateAnimationPropsBasedOnMEdiaQuery = (animationProps)=>{
-
-    if(window.innerWidth > 1200) return {
-      scale:animationProps.scale,
-      xPercent:animationProps.xPercent,
-      yPercent:animationProps.yPercent
-    };
-
-
-    const scaleFactor = (window.innerWidth/1200)*1.3
-    const xFactor = (window.innerWidth/1200)
-
-
+    const scaleFactor = (window.innerWidth / 1200) * 1.3;
     let scale = animationProps.scale * scaleFactor;
-    let yPercent = animationProps.ySmallPercent ?? animationProps.yPercent;
-
-    let xPercent = animationProps.xPercent /* * xFactor */;
-
-    if(animationProps.xSmallPercent){
-      xPercent = animationProps.xSmallPercent;
-    }else if(xPercent > 0){
-      xPercent -= (xFactor * 100*4)
-    }else{
-      xPercent += (xFactor * 100*4)
-    }
 
 
     return {
-      scale,
-      xPercent,
-      yPercent
-    }
-  }
+      ...animationProps,
+      scale
+    };
+  };
 
   useGSAP(() => {
     let ctx = gsap.context(() => {
-
       imageRefs.current.forEach((imageRef) => {
         gsap.set(imageRef, {
           scale: 1,
-          position: "absolute",
+          x: window.innerWidth/2 - imageRef.offsetWidth /2 ,
+          y: window.innerHeight/2- imageRef.offsetHeight /4
         });
       });
-
+/**/ 
       imageRefs.current.forEach((imageRef, index) => {
         let animationProps = {};
 
         switch (index) {
           case 0:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.8, xPercent: -150, yPercent: -100 });
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.8,
+              x: 0,
+              y: 0,
+            });
             break;
-          case 1:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.85, xPercent: 50, yPercent: -100 });
+/*           case 1:
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.85,
+              xPercent: 50,
+              yPercent: -100,
+            });
             break;
           case 2:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.8, xPercent: -150, yPercent: 10 });
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.8,
+              xPercent: -150,
+              yPercent: 10,
+            });
             break;
           case 3:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.85, xPercent: 50, yPercent: 10 });
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.85,
+              xPercent: 50,
+              yPercent: 10,
+            });
             break;
-
 
           case 4:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.75,xSmallPercent:-30, xPercent: -70,ySmallPercent:-60, yPercent: -100 });
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.75,
+              xPercent: -70,
+              yPercent: -100,
+            });
             break;
           case 5:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.8,xSmallPercent:-65, xPercent: 5,ySmallPercent:-70, yPercent: -50 });
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.8,
+              xPercent: 5,
+              yPercent: -50,
+            });
             break;
           case 6:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.8,xSmallPercent:-62, xPercent: -100,ySmallPercent:-30, yPercent: -15 });
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.8,
+              xPercent: -100,
+              yPercent: -15,
+            });
             break;
           case 7:
-            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({ scale: 0.8,xSmallPercent:-35, xPercent: -40,ySmallPercent:-20, yPercent: 10 });
-            break;
+            animationProps = calculateAnimationPropsBasedOnMEdiaQuery({
+              scale: 0.8,
+              xPercent: -40,
+              yPercent: 10,
+            });
+            break; */
         }
 
         gsap.to(imageRef, {
@@ -135,7 +145,7 @@ const StudentsGallarySection = forwardRef(({ scrollAreaRef }, ref) => {
             scrub: true,
           },
         });
-      });
+      }); 
     }, imageRefs);
 
     return () => ctx.revert();
@@ -144,32 +154,22 @@ const StudentsGallarySection = forwardRef(({ scrollAreaRef }, ref) => {
   return (
     <div
       ref={ref}
-      className="row justify-content-center align-content-center vw-100"
+      className="row  student-gallary-section vw-100 vh-100"
       style={{
         position: "relative",
-        minHeight: "100vh",
       }}
     >
-      <div className="container mt-0 p-0 student-gallary-section">
-        <div className="row justify-content-center text-center mt-5 mt-sm-0">
+      {studentsGallary.map((item, index) => {
+        return (
           <div
-            className="col-11 col-sm-8 col-md-7 col-lg-5"
-            style={{ position: "relative" }}
+            key={`student-gallary-img-${index}`}
+            className="student-gallary-img-container p-0"
+            ref={addToRefs}
           >
-            {studentsGallary.map((item, index) => {
-              return (
-                <div
-                  key={`student-gallary-img-${index}`}
-                  className="student-gallary-img-container"
-                  ref={addToRefs}
-                >
-                  <img src={item.img} />
-                </div>
-              );
-            })}
+            <img src={item.img} />
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 });
