f86b21be9537b4b527a07a9837624d6e82e55054
hesham.saleh <hesham.saleh@solve-it-solutions.com>
2024-11-02 00:46:52 +0200

    impl three fonts

--- server.js
+++ server.js
@@ -80,6 +80,11 @@ app.get(['/en/favicon.ico', '/ar/favicon.ico'], (req, res) => {
   res.sendFile(path.join(__dirname, `build_${lang}`, 'favicon.ico'));
 });
 
+app.get('/three-fonts/*', (req, res) => {
+  const afterkey = req.path.split('/three-fonts/')[1];
+  res.sendFile(path.join(__dirname, `build_en`, 'three-fonts',afterkey));
+});
+
 // Fallback to index.html for client-side routing in English
 app.get('/en/*', async (req, res) => {
   try {
