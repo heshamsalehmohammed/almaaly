4edd1fe3e3caf8cc645a47f20d6e49425620dfee
hesham.saleh <hesham.saleh@solve-it-solutions.com>
2024-11-04 02:12:11 +0200

    some refactor

--- src/components/Sections/QuotesSection.jsx
+++ src/components/Sections/QuotesSection.jsx
@@ -10,16 +10,15 @@ import "./QuotesSection.css";
 import {gsap,ScrollTrigger} from "../../gsapSetup";
 
 
-const Quote = forwardRef(({ img, top, text, author, title }, ref) => {
+const Quote = forwardRef(({ img, text, author, title ,className}, ref) => {
   return (
     <div
       ref={ref}
-      className="row justify-content-center align-items-center flex-sm-row px-0 px-sm-5 py-3 py-sm-5 mb-5"
+      className={`quote ${className} row justify-content-center align-items-center flex-sm-row px-0 px-sm-5 py-3 py-sm-5 mb-5`}
       style={{
         backgroundColor: "#fff",
         borderRadius: "25px",
         position: "absolute",
-        top,
       }}
     >
       <div className="col-10 col-md-6">
@@ -54,42 +53,7 @@ const QuotesSection = forwardRef(({ scrollAreaRef, config }, ref) => {
   useImperativeHandle(ref, () => quotesSectionRef.current);
 
   useEffect(() => {
-    // Function to set initial GSAP properties based on media queries
-    const applyGsapSet = () => {
-      gsap.set(quote1Ref.current, {
-        position: 'absolute',
-        top: '0',
-      });
-  
-      if (window.matchMedia('(max-width: 575.98px)').matches) {
-        gsap.set(quote2Ref.current, { position: 'absolute', top: `100vh` });
-        gsap.set(quote3Ref.current, { position: 'absolute', top: `200vh` });
-        gsap.set(quote4Ref.current, { position: 'absolute', top: `300vh` });
-        gsap.set(quotesSectionRef.current, { height: `400vh` });
-      } else if (window.matchMedia('(min-width: 576px) and (max-width: 767.98px)').matches) {
-        // Small devices (sm)
-        gsap.set(quote2Ref.current, { position: 'absolute', top: `100vh` });
-        gsap.set(quote3Ref.current, { position: 'absolute', top: `200vh` });
-        gsap.set(quote4Ref.current, { position: 'absolute', top: `300vh` });
-        gsap.set(quotesSectionRef.current, { height: `400vh` });
-      } else if (window.matchMedia('(min-width: 768px) and (max-width: 991.98px)').matches) {
-        // Medium devices (md)
-        gsap.set(quote2Ref.current, { position: 'absolute', top: `90vh` });
-        gsap.set(quote3Ref.current, { position: 'absolute', top: `180vh` });
-        gsap.set(quote4Ref.current, { position: 'absolute', top: `270vh` });
-        gsap.set(quotesSectionRef.current, { height: `360vh` });
-      } else if (window.matchMedia('(min-width: 992px)').matches) {
-        // Large devices and above (lg, xl)
-        gsap.set(quote2Ref.current, { position: 'absolute', top: `80vh` });
-        gsap.set(quote3Ref.current, { position: 'absolute', top: `160vh` });
-        gsap.set(quote4Ref.current, { position: 'absolute', top: `240vh` });
-        gsap.set(quotesSectionRef.current, { height: `320vh` });
-      }
-    };
-  
-    // Apply initial GSAP settings
-    applyGsapSet();
-  
+
     // Animate quote 1
     gsap.to(quote1Ref.current, {
       scrollTrigger: {
@@ -185,40 +149,14 @@ const QuotesSection = forwardRef(({ scrollAreaRef, config }, ref) => {
 
   return (
     <>
-      {/*       <Helmet>
-      
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "CreativeWorkSeries",
-            name: "Testimonials",
-            about: config.school.name,
-            mainEntity: quotes.map((quote) => ({
-              "@type": "Review",
-              reviewBody: quote.text,
-              author: {
-                "@type": "Person",
-                name: quote.author,
-                jobTitle: quote.title,
-              },
-              associatedMedia: {
-                "@type": "ImageObject",
-                contentUrl: quote.img,
-                description: `${quote.author} - ${quote.title}`
-              },
-            })),
-          })}
-        </script>
-      </Helmet> */}
-
       <div
         ref={quotesSectionRef}
-        className="row justify-content-center vw-100"
+        className="row justify-content-center vw-100 quotes-section"
         style={{
           position: "relative",
         }}
       >
-        <div className="container mt-0 p-0 quotes-section">
+        <div className="container mt-0 p-0 ">
           <div className="row justify-content-center text-center mt-5 mt-sm-0 ms-0 me-0 px-3">
             <div
               className="col-11 col-sm-10 col-md-9 col-lg-8 col-xl-7"
@@ -230,6 +168,7 @@ const QuotesSection = forwardRef(({ scrollAreaRef, config }, ref) => {
                 text={quotes[0].text}
                 author={quotes[0].author}
                 title={quotes[0].title}
+                className='quote-1'
               />
               <Quote
                 ref={quote2Ref}
@@ -237,6 +176,7 @@ const QuotesSection = forwardRef(({ scrollAreaRef, config }, ref) => {
                 text={quotes[1].text}
                 author={quotes[1].author}
                 title={quotes[1].title}
+                className='quote-2'
               />
               <Quote
                 ref={quote3Ref}
@@ -244,6 +184,7 @@ const QuotesSection = forwardRef(({ scrollAreaRef, config }, ref) => {
                 text={quotes[2].text}
                 author={quotes[2].author}
                 title={quotes[2].title}
+                className='quote-3'
               />
               <Quote
                 ref={quote4Ref}
@@ -251,6 +192,7 @@ const QuotesSection = forwardRef(({ scrollAreaRef, config }, ref) => {
                 text={quotes[3].text}
                 author={quotes[3].author}
                 title={quotes[3].title}
+                className='quote-4'
               />
             </div>
           </div>
